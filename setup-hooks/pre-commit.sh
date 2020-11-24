f02b08fea4a2824f86d8d30faf4b432eb414b262
Etienne Schmitt <etienne@schmitt-etienne.fr>
2020-11-24 16:10:43 +0100

    Bloquer les noms de fichier non ascii

--- setup-hooks/pre-commit.sh
+++ setup-hooks/pre-commit.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
-exit 1
+### Bloquage des noms de fichiers non ascii
+
+# Désactive le verificateur de shell pour la commande ci-dessous
+# shellcheck disable=SC2046
+if [ $(git diff --cached --name-only --diff-filter=A -z HEAD | LC_ALL=C tr -d '[ -~]\0' | wc -c) != 0 ]; then
+  cat <<\EOF
+Error: Attempt to add a non-ASCII file name.
+This is banned in this project...
+EOF
+  exit 1
+fi
