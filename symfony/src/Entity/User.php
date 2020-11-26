5173b54cfcd9ded236005de125843f4405b4e714
Calise <julien.calise@orange.fr>
2020-11-26 11:24:17 +0100

    Modification User / ajout Migration

--- symfony/src/Entity/User.php
+++ symfony/src/Entity/User.php
@@ -3,8 +3,7 @@
 namespace App\Entity;
 
 use App\Repository\UserRepository;
-use DateTimeImmutable;
-use DateTimeZone;
+use DateTimeInterface;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
 
@@ -50,6 +49,67 @@ class User implements UserInterface
      * @ORM\Column(type="datetimetz")
      */
     private $timezone;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $creationDate;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $birthDate;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $firstName;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $lastName;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $mail;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $activatedSince;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $suspended;
+
+    /**
+     * @ORM\Column(type="date",  nullable=true)
+     */
+    private $suspendedSince;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $deleted;
+
+    /**
+     * @ORM\Column(type="date", nullable=true)
+     */
+    private $deletedSince;
+
+    /**
+     * @ORM\OneToOne(targetEntity=Wallet::class, cascade={"persist", "remove"})
+     */
+    private $wallet;
+
+    /**
+     * @ORM\OneToOne(targetEntity=Cart::class, cascade={"persist", "remove"})
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $cart;
 
     public function getId(): ?int
     {
@@ -148,14 +208,158 @@ class User implements UserInterface
         return $this;
     }
 
-    public function getTimezone(): ?\DateTimeInterface
+    public function getTimezone(): ?DateTimeInterface
     {
         return $this->timezone;
     }
 
-    public function setTimezone(\DateTimeInterface $timezone): self
+    public function setTimezone(DateTimeInterface $timezone): self
     {
         $this->timezone = $timezone;
+
+        return $this;
+    }
+
+    public function getCreationDate(): ?DateTimeInterface
+    {
+        return $this->creationDate;
+    }
+
+    public function setCreationDate(DateTimeInterface $creationDate): self
+    {
+        $this->creationDate = $creationDate;
+
+        return $this;
+    }
+
+    public function getBirthDate(): ?DateTimeInterface
+    {
+        return $this->birthDate;
+    }
+
+    public function setBirthDate(DateTimeInterface $birthDate): self
+    {
+        $this->birthDate = $birthDate;
+
+        return $this;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(string $firstName): self
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName(string $lastName): self
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    public function getMail(): ?string
+    {
+        return $this->mail;
+    }
+
+    public function setMail(string $mail): self
+    {
+        $this->mail = $mail;
+
+        return $this;
+    }
+
+    public function getActivatedSince(): ?DateTimeInterface
+    {
+        return $this->activatedSince;
+    }
+
+    public function setActivatedSince(DateTimeInterface $activatedSince): self
+    {
+        $this->activatedSince = $activatedSince;
+
+        return $this;
+    }
+
+    public function getSuspended(): ?bool
+    {
+        return $this->suspended;
+    }
+
+    public function setSuspended(bool $suspended): self
+    {
+        $this->suspended = $suspended;
+
+        return $this;
+    }
+
+    public function getSuspendedSince(): ?DateTimeInterface
+    {
+        return $this->suspendedSince;
+    }
+
+    public function setSuspendedSince(DateTimeInterface $suspendedSince): self
+    {
+        $this->suspendedSince = $suspendedSince;
+
+        return $this;
+    }
+
+    public function getDeleted(): ?bool
+    {
+        return $this->deleted;
+    }
+
+    public function setDeleted(bool $deleted): self
+    {
+        $this->deleted = $deleted;
+
+        return $this;
+    }
+
+    public function getDeletedSince(): ?DateTimeInterface
+    {
+        return $this->deletedSince;
+    }
+
+    public function setDeletedSince(?DateTimeInterface $deletedSince): self
+    {
+        $this->deletedSince = $deletedSince;
+
+        return $this;
+    }
+
+    public function getWallet(): ?Wallet
+    {
+        return $this->wallet;
+    }
+
+    public function setWallet(?Wallet $wallet): self
+    {
+        $this->wallet = $wallet;
+
+        return $this;
+    }
+
+    public function getCart(): ?Cart
+    {
+        return $this->cart;
+    }
+
+    public function setCart(Cart $cart): self
+    {
+        $this->cart = $cart;
 
         return $this;
     }
