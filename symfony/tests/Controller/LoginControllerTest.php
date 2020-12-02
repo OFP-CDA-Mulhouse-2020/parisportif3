ab27a140031ac9be556b957fe5b3a3ba967b50b0
Calise <julien.calise@orange.fr>
2020-12-02 11:42:10 +0100

    Ajout Login
    - Ajout controller/class : LoginController.php / LoginAuthenticator.php
    - Ajout Test login : Show Form / Show Input

--- symfony/tests/Controller/LoginControllerTest.php
+++ symfony/tests/Controller/LoginControllerTest.php
@@ -6,14 +6,28 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class LoginControllerTest extends WebTestCase
 {
-    final public function testShowLogin()
+    final public function testShowLogin(): void
     {
         $client = static::createClient();
         $client->request('GET', '/login');
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
     }
 
-    final public function testShowForm()
+    final public function testShowForm(): void
     {
+        $client = static::createClient();
+        $client->request('GET', '/login');
+        self::assertSelectorExists('form');
+        self::assertSelectorExists('form > input');
+        self::assertSelectorExists('form > button');
+    }
+
+    final public function testShowInput(): void
+    {
+        $client = static::createClient();
+        $client->request('GET', '/login');
+        self::assertSelectorExists('input#inputUsername');
+        self::assertSelectorExists('input#inputPassword');
+        self::assertSelectorExists("button[type='submit']");
     }
 }
