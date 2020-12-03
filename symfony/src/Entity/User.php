79d1076615e18c9a67622ae951e373e6dcfe0077
Calise <julien.calise@orange.fr>
2020-12-03 17:11:05 +0100

    Modification User
    Ajout attribut Verified, avec les méthodes associées.

--- symfony/src/Entity/User.php
+++ symfony/src/Entity/User.php
@@ -130,6 +130,12 @@ final class User implements UserInterface
     /** @ORM\Column(type="date", nullable=true) */
     private ?DateTimeInterface $deletedAt;
 
+    /** @ORM\Column(type="boolean") */
+    private bool $verified = false;
+
+    /** @ORM\Column(type="date", nullable=true) */
+    private ?DateTimeInterface $verifiedAt;
+
 
     public function __construct()
     {
