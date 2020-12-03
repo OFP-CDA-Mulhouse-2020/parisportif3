b4e2e27f8cf936e16b03d33526283facf107a2f8
Calise <julien.calise@orange.fr>
2020-12-03 17:36:58 +0100

    Modification type de retour

--- symfony/src/Form/RegistrationFormType.php
+++ symfony/src/Form/RegistrationFormType.php
@@ -14,18 +14,24 @@ use Symfony\Component\Validator\Constraints\NotBlank;
 
 class RegistrationFormType extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options)
+    public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add('username')
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
-                'constraints' => [
-                    new IsTrue([
-                        'message' => 'You should agree to our terms.',
-                    ]),
-                ],
-            ])
+            ->add(
+                'agreeTerms',
+                CheckboxType::class,
+                [
+                    'mapped' => false,
+                    'constraints' => [
+                        new IsTrue(
+                            [
+                                'message' => 'You should agree to our terms.',
+                            ]
+                        ),
+                    ],
+                ]
+            )
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
@@ -45,10 +51,12 @@ class RegistrationFormType extends AbstractType
         ;
     }
 
-    public function configureOptions(OptionsResolver $resolver)
+    public function configureOptions(OptionsResolver $resolver): void
     {
-        $resolver->setDefaults([
-            'data_class' => User::class,
-        ]);
+        $resolver->setDefaults(
+            [
+                'data_class' => User::class,
+            ]
+        );
     }
 }
