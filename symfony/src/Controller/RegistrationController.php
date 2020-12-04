78e85480c97c27f92670d600839f99a7524dcf06
Calise <julien.calise@orange.fr>
2020-12-04 17:12:50 +0100

    Modification du form register -> ajout des champs name / birthdate / mdp confirmation / mail

--- symfony/src/Controller/RegistrationController.php
+++ symfony/src/Controller/RegistrationController.php
@@ -40,6 +40,7 @@ class RegistrationController extends AbstractController
                     $form->get('plainPassword')->getData()
                 )
             );
+            $user->setBirthDate($form->get('birthDate')->getData());
 
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($user);
--- symfony/src/Form/RegistrationFormType.php
+++ symfony/src/Form/RegistrationFormType.php
@@ -4,8 +4,12 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -17,7 +21,34 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('username')
+            ->add(
+                'username',
+                TextType::class,
+                [
+                    'required' => true,
+                    'constraints' => [
+                        new NotBlank(
+                            [
+                                'message' => "Veuillez entrer un nom d'utilisateur"
+                            ]
+                        )
+                    ]
+                ]
+            )
+            ->add(
+                'email',
+                EmailType::class,
+                [
+                    'required' => true,
+                    'constraints' => [
+                        new NotBlank(
+                            [
+                                'message' => 'Veuillez entrer un Email.'
+                            ]
+                        )
+                    ]
+                ]
+            )
             ->add(
                 'agreeTerms',
                 CheckboxType::class,
@@ -32,22 +63,72 @@ class RegistrationFormType extends AbstractType
                     ],
                 ]
             )
-            ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
-                'mapped' => false,
-                'constraints' => [
-                    new NotBlank([
-                        'message' => 'Please enter a password',
-                    ]),
-                    new Length([
-                        'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
-                        'max' => 4096,
-                    ]),
-                ],
-            ])
+            ->add(
+                'plainPassword',
+                RepeatedType::class,
+                [
+                    // instead of being set onto the object directly,
+                    // this is read and encoded in the controller
+                    'type' => PasswordType::class,
+                    'invalid_message' => 'Les deux mots de passe doivent être identiques.',
+                    'options' => ['attr' => ['class' => 'password-field']],
+                    'required' => true,
+                    'mapped' => false,
+                    'first_options' => ['label' => 'Password'],
+                    'second_options' => ['label' => 'Repeat Password'],
+                    'constraints' => [
+                        new NotBlank(
+                            [
+                                'message' => 'Veuillez entrer un mot de passe.',
+                            ]
+                        ),
+                        new Length(
+                            [
+                                'min' => 8,
+                                'minMessage' => 'Votre mot de passe doit contenir un minimum de  {{ limit }} caractère.',
+                                // max length allowed by Symfony for security reasons
+                                'max' => 4096,
+                            ]
+                        ),
+                    ],
+                ]
+            )
+            ->add(
+                'birthDate',
+                BirthdayType::class,
+                [
+                    'required' => true,
+                    'widget' => 'single_text'
+                ]
+            )
+            ->add(
+                'firstName',
+                TextType::class,
+                [
+                    'required' => true,
+                    'constraints' => [
+                        new NotBlank(
+                            [
+                                'message' => 'Veuillez entrer votre prénom.'
+                            ]
+                        )
+                    ]
+                ]
+            )
+            ->add(
+                'lastName',
+                TextType::class,
+                [
+                    'required' => true,
+                    'constraints' => [
+                        new NotBlank(
+                            [
+                                'message' => 'Veuillez entrer votre nom.'
+                            ]
+                        )
+                    ]
+                ]
+            )
         ;
     }
 
