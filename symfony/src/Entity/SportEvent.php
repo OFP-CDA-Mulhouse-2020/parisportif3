96de0ff088c7069b8f56d6e9cf24ac22e5803ba9
jannick <jannick.parmentier@sfr.fr>
2020-12-10 14:58:42 +0100

    Création de la class SportEvent

--- symfony/src/Entity/SportEvent.php
+++ symfony/src/Entity/SportEvent.php
@@ -8,17 +8,34 @@ use Doctrine\ORM\Mapping as ORM;
 /**
  * @ORM\Entity(repositoryClass=SportEventRepository::class)
  */
-class SportEvent
+final class SportEvent
 {
     /**
      * @ORM\Id
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
      */
-    private $id;
+    private int $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private string $location;
 
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getLocation(): ?string
+    {
+        return $this->location;
+    }
+
+    public function setLocation(string $location): self
+    {
+        $this->location = $location;
+
+        return $this;
     }
 }
