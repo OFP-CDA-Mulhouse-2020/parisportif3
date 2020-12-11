a77f3ab67780014b2f7f5a6e330fc3fe3147823b
jannick <jannick.parmentier@sfr.fr>
2020-12-11 13:32:43 +0100

    Création de l'entité CartTest

---
+++ symfony/tests/Entity/CartTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Tests\Entity;
+
+use PHPUnit\Framework\TestCase;
+
+final class CartTest extends TestCase
+{
+    //TODO faire la relation de génération avec la class Transaction
+
+    //TODO faire la relation opérer par la class User
+}
