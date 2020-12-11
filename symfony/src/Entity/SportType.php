a0fc1b394dc6f79b65278e1ffe9a62191e4de459
Calise <julien.calise@orange.fr>
2020-12-11 16:15:20 +0100

    Ajout du code/test pour les paramètres :
    - Name
    - NbrOfActiveAthlete

--- symfony/src/Entity/SportType.php
+++ symfony/src/Entity/SportType.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\SportTypeRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=SportTypeRepository::class)
@@ -15,10 +16,49 @@ class SportType
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
      */
-    private $id;
+    private int $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank
+     * @Assert\Regex(
+     *     pattern= "/^(?:\p{L}{2,}|E)(?:[ '-]\p{L}+)*$/u"
+     * )
+     */
+    private string $name;
+
+    /**
+     * @ORM\Column(type="integer")
+     * @Assert\GreaterThan(0)
+     */
+    private int $nbrOfActiveAthlete;
 
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): ?self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getNbrOfActiveAthlete(): ?int
+    {
+        return $this->nbrOfActiveAthlete;
+    }
+
+    public function setNbrOfActiveAthlete(int $nbrOfActiveAthlete): self
+    {
+        $this->nbrOfActiveAthlete = $nbrOfActiveAthlete;
+
+        return $this;
     }
 }
--- symfony/tests/Entity/SportTypeTest.php
+++ symfony/tests/Entity/SportTypeTest.php
@@ -3,13 +3,103 @@
 namespace App\Tests\Entity;
 
 use App\Entity\SportType;
-use PHPUnit\Framework\TestCase;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpKernel\KernelInterface;
 
-class SportTypeTest extends TestCase
+class SportTypeTest extends WebTestCase
 {
+    private function getKernel(): KernelInterface
+    {
+        $kernel = $this->bootKernel();
+        $kernel->boot();
+        return $kernel;
+    }
+
     // Doit pouvoir créer une instance de la classe SportType
     public function testCreateSportType(): void
     {
+        $sportType = new SportType();
+
         $this->assertInstanceOf(SportType::class, new SportType());
+    }
+
+    // Doit pouvoir ajouter un nom à notre type de sport.
+
+    /** @dataProvider validSportNameProvider */
+    public function testSetValidSportName(string $validNameProvider): void
+    {
+        $sportType = new SportType();
+        $sportType->setName($validNameProvider);
+
+        $kernel = $this->getKernel();
+        $validator = $kernel->getContainer()->get('validator');
+        $violations = $validator->validate($sportType);
+
+        $this->assertCount(0, $violations);
+    }
+
+    // Ne doit pas pouvoir ajouter un nom de sport invalide
+
+    /**
+     * @dataProvider invalidSportNameProvider
+     */
+    public function testSetInvalidSportName(string $invalidSportNameProvider): void
+    {
+        $sportType = new SportType();
+        $sportType->setName($invalidSportNameProvider);
+
+        $kernel = $this->getKernel();
+        $validator = $kernel->getContainer()->get('validator');
+        $violations = $validator->validate($sportType);
+
+        $this->assertGreaterThanOrEqual(1, count($violations));
+    }
+
+    public function testSetValidNumberOfActiveAthlete(): void
+    {
+        $sportType = new SportType();
+        $sportType->setNbrOfActiveAthlete(12);
+
+        $kernel = $this->getKernel();
+        $validator = $kernel->getContainer()->get('validator');
+        $violations = $validator->validate($sportType);
+
+        $this->assertCount(1, $violations);
+    }
+
+    public function testSetInvalidNumberOfActiveAthlete(): void
+    {
+        $sportType = new SportType();
+
+        $sportType->setNbrOfActiveAthlete(20);
+
+        $kernel = $this->getKernel();
+        $validator = $kernel->getContainer()->get('validator');
+        $violations = $validator->validate($sportType);
+
+        $this->assertGreaterThanOrEqual(1, count($violations));
+    }
+
+
+    public function invalidSportNameProvider(): array
+    {
+        return [
+            ['12Bilboquet'],
+            [''],
+            ['&Belotte Bretonne'],
+            [' '],
+            ['a'],
+        ];
+    }
+
+    public function validSportNameProvider(): array
+    {
+        return [
+            ['Karaté'],
+            ["Tir à l'arc"],
+            ["Jambon de Bayonne"],
+            ["E-Sport"],
+            ['Ju-Jitsû'],
+        ];
     }
 }
