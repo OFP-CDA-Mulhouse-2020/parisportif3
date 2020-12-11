35f9ebf0e700a9a23fc9678fec7842eab479c025
jannick <jannick.parmentier@sfr.fr>
2020-12-11 13:38:56 +0100

    Ajout du test : CartTest::testCreateTest

--- symfony/tests/Entity/CartTest.php
+++ symfony/tests/Entity/CartTest.php
@@ -2,10 +2,17 @@
 
 namespace App\Tests\Entity;
 
+use App\Entity\Cart;
 use PHPUnit\Framework\TestCase;
 
 final class CartTest extends TestCase
 {
+    public function testCreateCart(): void
+    {
+        $cart = new Cart();
+
+        $this->assertInstanceOf(Cart::class, $cart);
+    }
     //TODO faire la relation de génération avec la class Transaction
 
     //TODO faire la relation opérer par la class User
