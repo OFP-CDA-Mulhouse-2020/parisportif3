4f704d8fcafb37e5817dbe28a1533409743df1ef
jannick <jannick.parmentier@sfr.fr>
2020-12-15 11:09:50 +0100

    Finalisation des tests de BetChoiceTest

--- symfony/tests/Entity/BetChoiceTest.php
+++ symfony/tests/Entity/BetChoiceTest.php
@@ -3,14 +3,50 @@
 namespace App\Tests\Entity;
 
 use App\Entity\BetChoice;
-use PHPUnit\Framework\TestCase;
+use App\Tests\GeneralTestMethod;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\Validator\Validator\TraceableValidator;
 
-final class BetChoiceTest extends TestCase
+final class BetChoiceTest extends WebTestCase
 {
+    /**
+     * @var BetChoice
+     */
+    private BetChoice $betChoice;
+    /**
+     * @var mixed
+     */
+    private TraceableValidator $validator;
+
+    public function setUp(): void
+    {
+        $this->betChoice = new BetChoice();
+        $this->validator = GeneralTestMethod::getKernelAndValidator()["validator"];
+    }
+
     public function testCreateBetChoice(): void
     {
-        $betChoice = new BetChoice();
+        $choice = [1, 2, 3];
+        $this->betChoice->setChoice($choice);
+        $this->assertIsArray($this->betChoice->getChoice());
 
-        $this->assertInstanceOf(BetChoice::class, $betChoice);
+        $this->assertCount(0, $this->validator->validate($this->betChoice));
+    }
+
+    /** @dataProvider invalidBetChoiceProvider */
+    public function testInvalidBetChoice(array $invalidBetChoiceProvider): void
+    {
+        $this->betChoice->setChoice($invalidBetChoiceProvider);
+        $violations = $this->validator->validate($this->betChoice);
+
+        $this->assertGreaterThanOrEqual(1, count($violations));
+    }
+
+    public function invalidBetChoiceProvider(): array
+    {
+        return [
+            [['']],
+            [['juin']]
+        ];
     }
 }
