2cc4b42b2ca180fcb55c130c481143d5a26d0dfa
jannick <jannick.parmentier@sfr.fr>
2020-12-15 11:11:04 +0100

    Assertions pour lever les violations de la classe BetChoie

--- symfony/src/Entity/BetChoice.php
+++ symfony/src/Entity/BetChoice.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\BetChoiceRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=BetChoiceRepository::class)
@@ -17,8 +18,31 @@ final class BetChoice
      */
     private int $id;
 
+    /**
+     * @ORM\Column(type="array")
+     * @var array<int>
+     * @Assert\NotNull
+     * @Assert\All({
+     *     @Assert\NotBlank,
+     *     @Assert\Type("int")
+     * })
+     */
+    private array $choice = [];
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getChoice(): ?array
+    {
+        return $this->choice;
+    }
+
+    public function setChoice(array $choice): self
+    {
+        $this->choice = $choice;
+
+        return $this;
     }
 }
