5eea1143d4cd1249c0b70f96edf481eab8c3bb12
jannick <jannick.parmentier@sfr.fr>
2020-12-15 11:16:43 +0100

    Création du test testValidDate

--- symfony/tests/Entity/SportEventTest.php
+++ symfony/tests/Entity/SportEventTest.php
@@ -3,9 +3,13 @@
 namespace App\Tests\Entity;
 
 use App\Entity\SportEvent;
-use PHPUnit\Framework\TestCase;
+use DateInterval;
+use DateTime;
+use DateTimeZone;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpKernel\KernelInterface;
 
-final class SportEventTest extends TestCase
+final class SportEventTest extends WebTestCase
 {
     /*
      * Il doit avoir un nom de lieu, un nom pour la compétition
@@ -13,9 +17,14 @@ final class SportEventTest extends TestCase
      * Il ne doit pas accepté les caractères spéciaux et les nombres dans les noms
      */
 
-    /*
-     * Est-ce que je peux créer un SportEvent
-     */
+    private function getKernel(): KernelInterface
+    {
+        $kernel = $this->bootKernel();
+        $kernel->boot();
+        return $kernel;
+    }
+
+    //Est-ce que je peux créer un SportEvent
     public function testCreateSportEvent(): void
     {
         $sportEvent = new SportEvent();
@@ -23,9 +32,7 @@ final class SportEventTest extends TestCase
         $this->assertInstanceOf(SportEvent::class, $sportEvent);
     }
 
-    /*
-     * Est-ce que SportEvent contient un lieu valide
-     */
+    //Est-ce que SportEvent contient un lieu valide
     public function testLocationInputIsReturned(): void
     {
         $sportEvent = new SportEvent();
@@ -38,9 +45,7 @@ final class SportEventTest extends TestCase
 
     //TODO adresse valide avec API
 
-    /*
-     * Est-ce que SportEvent contient une competition
-     */
+    //Est-ce que SportEvent contient une competition
     public function testCompetitionInputIsReturned(): void
     {
         $sportEvent = new SportEvent();
@@ -50,4 +55,26 @@ final class SportEventTest extends TestCase
 
         $this->assertSame($competition, $sportEvent->getCompetition());
     }
+
+    /*
+    Est-ce que la date est supérieure de 2 jours à la date actuelle
+    et que l'heure est supérieur de 48H à l'heure actuelle
+    */
+    public function testValidDate(): void
+    {
+        $sportEvent = new SportEvent();
+        $date = new DateTime('now', new DateTimeZone('Europe/Paris'));
+        $date->add(new DateInterval('P4D'));
+
+        $kernel = $this->getKernel();
+        $validator = $kernel->getContainer()->get('validator');
+        $violations = $validator->validate($sportEvent);
+
+        $sportEvent->setDate($date);
+        $this->assertSame($date, $sportEvent->getDate());
+        $this->assertCount(0, $violations);
+    }
+
+    //Est-ce que la DateTimeZone est la bonne !
+    //TODO finir le test sur Timezone
 }
