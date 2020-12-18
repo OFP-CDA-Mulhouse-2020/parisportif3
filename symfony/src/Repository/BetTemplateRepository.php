51b6124d0e9161ea47536758034f74f99cfa844b
Etienne Schmitt <etienne@schmitt-etienne.fr>
2020-12-18 11:21:03 +0100

    Correction de la classe BetTemplateRepository
    Classe transformer en classe final
    Ajout des informations d'extends pour phpstan

--- symfony/src/Repository/BetTemplateRepository.php
+++ symfony/src/Repository/BetTemplateRepository.php
@@ -7,12 +7,15 @@ use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
+ * @template TEntityClass of object
+ * @extends \Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository<TEntityClass>
+ *
  * @method BetTemplate|null find($id, $lockMode = null, $lockVersion = null)
  * @method BetTemplate|null findOneBy(array $criteria, array $orderBy = null)
  * @method BetTemplate[]    findAll()
  * @method BetTemplate[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
-class BetTemplateRepository extends ServiceEntityRepository
+final class BetTemplateRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)
     {
