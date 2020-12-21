10f754c381bb12a9c6d01e6170730fc47270263b
Calise <julien.calise@orange.fr>
2020-12-21 15:30:52 +0100

    Modification BetTemplateChoice:
    - Ajout Setup Instance Classe/ Validator

--- symfony/tests/Entity/BetTemplateChoiceTest.php
+++ symfony/tests/Entity/BetTemplateChoiceTest.php
@@ -3,10 +3,21 @@
 namespace App\Tests\Entity;
 
 use App\Entity\BetTemplateChoice;
+use App\Tests\GeneralTestMethod;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\Validator\Validator\TraceableValidator;
 
 class BetTemplateChoiceTest extends WebTestCase
 {
+    private BetTemplateChoice $betTemplateChoice;
+    private TraceableValidator $validator;
+
+    public function setUp(): void
+    {
+        $this->validator = GeneralTestMethod::getKernelAndValidator()['validator'];
+        $this->betTemplateChoice = new BetTemplateChoice();
+    }
+
     //Doit pouvoir créer une instance de la classe BetTemplateChoice
     public function testCreateBetTemplateChoiceObject(): void
     {
@@ -19,13 +30,15 @@ class BetTemplateChoiceTest extends WebTestCase
     //Doit mettre à jour une description
     public function testCanUpdateBetTemplateChoice(): void
     {
-        $BetTemplateChoice = new BetTemplateChoice();
         $UpdatedBetTemplateChoice = [
             "Équipe 1" => "Marseille",
             "Équipe 2" => "PSG",
             "Résultat" => "1 Gagne"
         ];
 
-        $BetTemplateChoice->updateDescription($UpdatedBetTemplateChoice);
+        $violations = $this->validator->validate($this->betTemplateChoice);
+        $this->betTemplateChoice->updateDescription($UpdatedBetTemplateChoice);
+
+        $this->assertCount(0, $violations);
     }
 }
