dbdeadea8575cb471d2106c277da8136665aabca
Etienne Schmitt <etienne@schmitt-etienne.fr>
2020-12-22 11:03:14 +0100

    Ajout du test pour valider la valeur valide et invalide de Bet::amount

---
+++ symfony/src/Entity/Bet.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BetRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity(repositoryClass=BetRepository::class)
+ */
+final class Bet
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private int $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     * @Assert\GreaterThanOrEqual(1)
+     */
+    private int $amount;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getAmount(): ?int
+    {
+        return $this->amount;
+    }
+
+    public function setAmount(int $amount): self
+    {
+        $this->amount = $amount;
+
+        return $this;
+    }
+}
--- symfony/tests/Entity/BetTest.php
+++ symfony/tests/Entity/BetTest.php
@@ -2,11 +2,13 @@
 
 namespace App\Tests\Entity;
 
+use App\Entity\Bet;
 use App\Tests\GeneralTestMethod;
-use PHPUnit\Framework\TestCase;
+use Generator;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\Validator\Validator\TraceableValidator;
 
-final class BetTest extends TestCase
+final class BetTest extends WebTestCase
 {
     private TraceableValidator $validator;
     private Bet $bet;
@@ -20,5 +22,44 @@ final class BetTest extends TestCase
     public function testCartItemClassExist(): void
     {
         $this->assertInstanceOf(Bet::class, $this->bet);
+    }
+
+    /** @dataProvider validAmountProvider */
+    public function testAmountIsEnough(int $value): void
+    {
+        $this->bet->setAmount($value);
+
+        $violations = $this->validator->validate($this->bet);
+
+        $this->assertCount(0, $violations);
+    }
+
+    /** @return Generator<array<int>> */
+    public function validAmountProvider(): Generator
+    {
+        yield [420];
+        yield [120];
+        yield [40];
+        yield [50];
+        yield [1];
+    }
+
+    /** @dataProvider invalidAmountProvider */
+    public function testAmountIsInvalid(int $value): void
+    {
+        $this->bet->setAmount($value);
+
+        $violations = $this->validator->validate($this->bet);
+        $this->assertGreaterThanOrEqual(1, count($violations));
+    }
+
+    /** @return Generator<array<int>> */
+    public function invalidAmountProvider(): Generator
+    {
+        yield [0];
+        yield [-50];
+        yield [-1400];
+        yield [-1];
+        yield [-420];
     }
 }
