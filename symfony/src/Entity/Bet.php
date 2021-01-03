50bd3db15544335a8887d74c3cefdc41697b1b9e
Etienne Schmitt <etienne@schmitt-etienne.fr>
2021-01-03 21:19:47 +0100

    Ajout de l'attribut status dans l'entité Bet

--- symfony/src/Entity/Bet.php
+++ symfony/src/Entity/Bet.php
@@ -27,6 +27,25 @@ final class Bet
      */
     private int $amount;
 
+    /**
+     * @ORM\Column(type="integer")
+     *
+     * @Assert\NotNull
+     * @Assert\NotBlank
+     */
+    private int $status;
+
+    public const STATUS_UNPAID = 0;
+    public const STATUS_PENDING = 1;
+    public const STATUS_PAID = 2;
+
+    /** @var array|string[] */
+    public static array $statusList = [
+        self::STATUS_UNPAID => "STATUS_UNPAID",
+        self::STATUS_PENDING => "STATUS_PENDING",
+        self::STATUS_PAID => "STATUS_PAID"
+    ];
+
     public function getId(): ?int
     {
         return $this->id;
@@ -40,6 +59,18 @@ final class Bet
     public function setAmount(int $amount): self
     {
         $this->amount = $amount;
+
+        return $this;
+    }
+
+    public function getStatus(): ?int
+    {
+        return $this->status;
+    }
+
+    public function setStatus(int $status): self
+    {
+        $this->status = $status;
 
         return $this;
     }
