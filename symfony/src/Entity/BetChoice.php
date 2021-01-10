b3adb0628d4efc597f590227e795e5c77d0b2efb
Etienne Schmitt <etienne@schmitt-etienne.fr>
2021-01-10 15:23:27 +0100

    BetChoice: L'assertion est maintenant réalisé avec une assertion Callback dans la méthode BetChoice::validateChoice()

--- symfony/src/Entity/BetChoice.php
+++ symfony/src/Entity/BetChoice.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use App\Repository\BetChoiceRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Context\ExecutionContextInterface;
 
 /**
  * @ORM\Entity(repositoryClass=BetChoiceRepository::class)
@@ -21,42 +22,49 @@ final class BetChoice
     /**
      * @ORM\Column(type="array")
      *
-     * @TODO Corriger les assertions pour coller à l'UML
-     *
-     * @var array<int>
-     * @Assert\NotNull
-     * @Assert\NotBlank
-     * @Assert\All({
-     *     @Assert\GreaterThan(0),
-     *     @Assert\NotNull,
-     *     @Assert\NotBlank,
-     *     @Assert\Type("int")
-     * })
+     * @var array<string, int>
      */
     private array $choice = [];
+
+    //TODO Ajouter la relation avec BetTemplateChoice
+
 
     public function getId(): ?int
     {
         return $this->id;
     }
 
-    /**
-     * @TODO Corriger pour coller à l'UML
-     * @return array<int>
-     */
+    /** @return array<string, int> */
     public function getChoice(): ?array
     {
         return $this->choice;
     }
 
-    /**
-     * @TODO Corriger pour coller à l'UML
-     * @param array<int> $choice
-     */
+    /** @param array<string, int> $choice */
     public function setChoice(array $choice): self
     {
         $this->choice = $choice;
 
         return $this;
+    }
+
+    /**
+     * @Assert\Callback()
+     * @TODO Ajouter le test avec BetTemplateChoice
+     */
+    public function validateChoice(ExecutionContextInterface $context): void
+    {
+        foreach ($this->choice as $bet => $choice) {
+            if (!is_string($bet)) {
+                $context->buildViolation("Bet is not a string")
+                    ->atPath("choice")
+                    ->addViolation();
+            }
+            if (!is_int($choice)) {
+                $context->buildViolation("Choice is not a int")
+                    ->atPath("choice")
+                    ->addViolation();
+            }
+        }
     }
 }
