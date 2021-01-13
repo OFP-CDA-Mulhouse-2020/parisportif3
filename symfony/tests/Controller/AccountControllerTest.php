b7e82815e9a7f0e6a66d492de949d7fa05c7ea56
Calise <julien.calise@orange.fr>
2021-01-13 10:23:41 +0100

    Modification de AccountControllerTest.php :
    - Ajout des tests pour le form

--- symfony/tests/Controller/AccountControllerTest.php
+++ symfony/tests/Controller/AccountControllerTest.php
@@ -11,18 +11,20 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 class AccountControllerTest extends WebTestCase
 {
     private KernelBrowser $client;
-    private User $validUser;
+    private User $noModifUser;
+    private User $ModifUser;
 
     public function setUp(): void
     {
         $this->client = GeneralTestMethod::getClient();
         $userRepo = static::$container->get(UserRepository::class);
-        $this->validUser = $userRepo->findOneBy(['email' => 'test@test.com']);
+        $this->noModifUser = $userRepo->findOneBy(['username' => 'test0']);
+        $this->ModifUser = $userRepo->findOneBy(['username' => 'test1']);
     }
 
     public function testShowMyAccount(): void
     {
-        $this->client->loginUser($this->validUser);
+        $this->client->loginUser($this->noModifUser);
         $this->client->request('GET', '/myAccount');
         $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
     }
@@ -31,5 +33,51 @@ class AccountControllerTest extends WebTestCase
     {
         $this->client->request('GET', '/myAccount');
         $this->assertEquals(302, $this->client->getResponse()->getStatusCode());
+    }
+
+    public function testShowPersonalInfoForm(): void
+    {
+        $this->client->loginUser($this->noModifUser);
+        $this->client->request('GET', 'myAccount/personalInfo');
+
+        $this->assertSelectorExists('form');
+        $this->assertSelectorExists('form > input');
+        $this->assertSelectorExists('form > button');
+    }
+
+    public function testShowPersonalInfoFromUser(): void
+    {
+        $this->client->loginUser($this->noModifUser);
+        $crawler = $this->client->request('GET', 'myAccount/personalInfo');
+        $form = $crawler->selectButton('Soumettre')->form();
+
+        $this->assertSame($this->noModifUser->getEmail(), $form['personal_info_form[email]']->getValue());
+        $this->assertSame($this->noModifUser->getLastName(), $form['personal_info_form[lastName]']->getValue());
+        $this->assertSame($this->noModifUser->getFirstName(), $form['personal_info_form[firstName]']->getValue());
+        $this->assertSame($this->noModifUser->getTimeZone(), $form['personal_info_form[timeZone]']->getValue());
+    }
+
+    public function testModifyUserWithInvalidInfo(): void
+    {
+        $this->client->loginUser($this->ModifUser);
+        $crawler = $this->client->request('GET', 'myAccount/personalInfo');
+        $form = $crawler->selectButton('Soumettre')->form();
+
+        $form['personal_info_form[email]']->setValue('1234568');
+
+        $this->client->submit($form);
+        $this->assertSelectorExists('.flash-warning');
+    }
+
+    public function testModifyUserWithValidInfo(): void
+    {
+        $this->client->loginUser($this->ModifUser);
+        $crawler = $this->client->request('GET', 'myAccount/personalInfo');
+        $form = $crawler->selectButton('Soumettre')->form();
+
+        $form['personal_info_form[email]']->setValue('jean@test.com');
+
+        $this->client->submit($form);
+        $this->assertSelectorExists('.flash-success');
     }
 }
