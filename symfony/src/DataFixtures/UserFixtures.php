cc8bd361b97641ecfddd4f471259e448f16e9d26
Calise <julien.calise@orange.fr>
2021-01-13 10:16:47 +0100

    Modifications de UserFixtures :
    Créations de plusieurs entité User, pour avoir une entité fixe, et une modifiable.

--- symfony/src/DataFixtures/UserFixtures.php
+++ symfony/src/DataFixtures/UserFixtures.php
@@ -22,21 +22,22 @@ class UserFixtures extends Fixture
 
     public function load(ObjectManager $manager): void
     {
-        $user = new User();
-        $user->setWallet(new Wallet());
-        $user->setUsername('Test');
-        $user->setEmail('test@test.com');
-        $user->setBirthDate((new DateTime())->setDate(1995, 02, 15));
-        $user->setFirstName('Jean-Michele');
-        $user->setLastName('Dupont');
-        $user->setPassword(
-            $this->encoder->encodePassword(
-                $user,
-                'correct horse battery staple'
-            )
-        );
-
-        $manager->persist($user);
+        for ($i = 0; $i <= 1; $i++) {
+            $user = new User();
+            $user->setWallet(new Wallet());
+            $user->setUsername('Test' . $i);
+            $user->setEmail('test' . $i . '@test.com');
+            $user->setBirthDate((new DateTime())->setDate(1995, 02, 15));
+            $user->setFirstName('Jean-Michele');
+            $user->setLastName('Dupont');
+            $user->setPassword(
+                $this->encoder->encodePassword(
+                    $user,
+                    'correct horse battery staple'
+                )
+            );
+            $manager->persist($user);
+        }
         $manager->flush();
     }
 }
