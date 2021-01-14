5198245735e163993c0ccbdaae63c4887dbc998d
Calise <julien.calise@orange.fr>
2021-01-14 16:16:04 +0100

    Refactoring d'AccountController : -> Création de la méthode verifyIfUserIsConnected -> Création de la méthode handleForm
    Changement de visibilité des méthodes de AccountController

--- symfony/src/Controller/AccountController.php
+++ symfony/src/Controller/AccountController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Form\PersonalInfoFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,13 +16,10 @@ class AccountController extends AbstractController
      */
     public function index(): Response
     {
-        if (!$this->getUser()) {
-            $this->addFlash(
-                'warning',
-                "Pour accéder à cette page, vous devez d'abord vous connecter."
-            );
+        if ($this->verifyIfUserIsConnected()) {
             return $this->redirectToRoute('app_login');
         }
+
         return $this->render(
             'account/index.html.twig',
             [
@@ -30,52 +28,60 @@ class AccountController extends AbstractController
         );
     }
 
-    /**
-     * @Route("/myAccount/personalInfo", name="personalInfo")
-     */
-    public function UpdatePersonalInfo(Request $request): Response
+    private function verifyIfUserIsConnected(): bool
     {
         if (!$this->getUser()) {
             $this->addFlash(
                 'warning',
                 "Pour accéder à cette page, vous devez d'abord vous connecter."
             );
-            return $this->redirectToRoute('home');
+            return true;
         }
+        return false;
+    }
 
+    private function handleForm(Request $request, array $flashMessage): FormInterface
+    {
         $form = $this->createForm(PersonalInfoFormType::class, $this->getUser());
         $form->handleRequest($request);
 
         if ($form->isSubmitted()) {
             if ($form->isValid()) {
                 $entityManager = $this->getDoctrine()->getManager();
-
-                $user = $form->getData();
-
-                $entityManager->persist($user);
+                $entityManager->persist($form->getData());
                 $entityManager->flush();
 
-                $this->addFlash('success', 'Votre compte a été mis à jour avec succès');
-
-                return $this->render(
-                    'account/index.html.twig',
-                    [
-                        'user' => $this->getUser(),
-                        'myAccount_list' => 'personalInfo',
-                        'personalInfo' => $form->createView()
-                    ]
-                );
+                $this->addFlash($flashMessage['success']['type'], $flashMessage['success']['message']);
+            } else {
+                $this->addFlash($flashMessage['fail']['type'], $flashMessage['fail']['message']);
             }
-            $this->addFlash('warning', "Une erreur est survenue lors de la mise à jour de votre profil");
-            return $this->render(
-                'account/index.html.twig',
-                [
-                    'user' => $this->getUser(),
-                    'myAccount_list' => 'personalInfo',
-                    'personalInfo' => $form->createView()
-                ]
-            );
         }
+        return $form;
+    }
+
+    /**
+     * @Route("/myAccount/personalInfo", name="personalInfo")
+     */
+    public function UpdatePersonalInfo(Request $request): Response
+    {
+        if ($this->verifyIfUserIsConnected()) {
+            return $this->redirectToRoute('app_login');
+        }
+
+        $form = $this->handleForm(
+            $request,
+            [
+                'success' => [
+                    'type' => 'success',
+                    'message' => 'Vos informations on bien été mis à jour'
+                ],
+                'fail' => [
+                    'type' => 'warning',
+                    'message' => 'Une erreur est survenue pendant la mise à jour de votre profil'
+                ]
+            ]
+        );
+
         return $this->render(
             'account/index.html.twig',
             [
@@ -85,6 +91,5 @@ class AccountController extends AbstractController
             ]
         );
     }
-
 
 }
