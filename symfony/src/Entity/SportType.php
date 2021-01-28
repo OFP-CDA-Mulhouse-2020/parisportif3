0a0f02775043e23d9720013d5d06f35f0f5a516f
saitokojiro <kazutonight@gmail.com>
2021-01-28 10:56:18 +0100

    mise a jour de l'entité SportType et de SportTypeCrud

--- symfony/src/Entity/SportType.php
+++ symfony/src/Entity/SportType.php
@@ -21,7 +21,7 @@ class SportType
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
      */
-    private ?int $id;
+    private ?int $id = null;
 
     /**
      * @ORM\Column(type="string", length=255)
