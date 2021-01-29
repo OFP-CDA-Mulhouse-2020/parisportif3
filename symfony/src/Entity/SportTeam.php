c2f548353435000bff4890be010c7dd03abd5b0d
saitokojiro <kazutonight@gmail.com>
2021-01-29 10:53:53 +0100

    creation des differant CRUD et correction des entity

--- symfony/src/Entity/SportTeam.php
+++ symfony/src/Entity/SportTeam.php
@@ -21,7 +21,7 @@ class SportTeam
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
      */
-    private ?int $id;
+    private ?int $id = null;
 
     /**
      * @ORM\Column(type="string", length=255)
