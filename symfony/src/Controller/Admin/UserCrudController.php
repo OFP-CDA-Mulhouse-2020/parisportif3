122d9cf95e5ca822a85d1a723920c32a53e3d10c
saitokojiro <kazutonight@gmail.com>
2021-02-05 16:19:15 +0100

    mise a jour de easy admin

---
+++ symfony/src/Controller/Admin/UserCrudController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Controller\Admin;
+
+
+//use App\Entity\SportType;
+use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class UserCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return User::class;
+    }
+
+    public function configureActions(Actions $actions): Actions
+    {
+        return $actions
+            ->add(Crud::PAGE_EDIT, Action::SAVE_AND_ADD_ANOTHER);
+    }
+
+    
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            //IdField::new('id'),
+
+
+            TextField::new('Username'),
+            TextField::new('password'),
+            TextField::new('email'),
+            TextField::new('lastName'),
+            TextField::new('FirstName'),
+            TextField::new('FirstName'),
+            //TextEditorField::new('description'),
+            BooleanField::new('verified'),
+            BooleanField::new('suspended'),
+            BooleanField::new('deleted')
+        ];
+    }
+    
+}
